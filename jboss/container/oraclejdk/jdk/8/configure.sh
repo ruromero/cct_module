080d6c36cd774631f8dae2192c51166d0d00c315
Ruben Romero Montes <rromerom@redhat.com>
2019-07-03 17:22:43 +0200

    Add oraclejdk module
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

---
+++ jboss/container/oraclejdk/jdk/8/configure.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Configure module
+set -e
+
+SCRIPT_DIR=$(dirname $0)
+ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
+
+# OracleJDK - BEGIN
+if [ ! $(command -v java) ]; then
+  echo "Oracle JDK must be installed manually in the image"
+  exit 1
+fi
+
+JBOSS_CONTAINER_MODULE=/opt/jboss/container/openjdk/jdk
+mkdir -p ${JBOSS_CONTAINER_MODULE}
+chown -R jboss:root ${JBOSS_CONTAINER_MODULE}
+# OracleJDK - END
+
+chown -R jboss:root $SCRIPT_DIR
+chmod -R ug+rwX $SCRIPT_DIR
+chmod ug+x ${ARTIFACTS_DIR}/opt/jboss/container/openjdk/jdk/*
+
+pushd ${ARTIFACTS_DIR}
+cp -pr * /
+popd
+
+echo securerandom.source=file:/dev/urandom >> /usr/lib/jvm/java/jre/lib/security/java.security
