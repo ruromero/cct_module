108bfceffdc2b55755b5dabb9b192264934d7c45
Ruben Romero Montes <rromerom@redhat.com>
2019-07-04 12:02:38 +0200

    Update alternatives
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- jboss/container/oraclejdk/jdk/8/configure.sh
+++ jboss/container/oraclejdk/jdk/8/configure.sh
@@ -10,19 +10,28 @@ ARTIFACTS_DIR=${SCRIPT_DIR}/artifacts
 rpm -e --nodeps $(rpm -qa | grep openjdk)
 ## Create openjdk-like alternatives and symlinks
 JDK_PATH=/usr/java/jdk1.8.0_211-amd64
+JDK_LINKS_PATH=/usr/lib/jvm/java-1.8.0-openjdk
+
+alternatives --install /usr/bin/java java ${JDK_PATH}/jre/bin/java 1
+alternatives --install /usr/bin/javac javac ${JDK_PATH}/bin/javac 1
+alternatives --install /usr/bin/java_sdk_1.8.0_openjdk java_sdk_1.8.0_openjdk ${JDK_PATH} 1
+alternatives --install /usr/bin/java java ${JDK_LINKS_PATH}/jre/bin/java 1
+alternatives --install /usr/bin/java_sdk_1.8.0 java_sdk_1.8.0 ${JDK_LINKS_PATH} 1
+alternatives --install /usr/bin/java_sdk_openjdk java_sdk_openjdk ${JDK_LINKS_PATH} 1
+alternatives --install /usr/bin/jre_1.8.0 jre_1.8.0 ${JDK_LINKS_PATH}/jre 1
+alternatives --install /usr/bin/jre_1.8.0_openjdk jre_1.8.0_openjdk ${JDK_LINKS_PATH}/jre 1
+alternatives --install /usr/bin/jre_openjdk jre_openjdk ${JDK_LINKS_PATH}/jre 1
+
 alternatives --install /usr/bin/java java ${JDK_PATH}/jre/bin/java 1 \
-  --slave /usr/bin/java_sdk java_sdk ${JDK_PATH} \
-  --slave /usr/bin/java_sdk_1.8.0 java_sdk_1.8.0 ${JDK_PATH} \
-  --slave /usr/bin/java_sdk_openjdk java_sdk_openjdk ${JDK_PATH} \
-  --slave /usr/bin/java_sdk_1.8.0_openjdk java_sdk_1.8.0_openjdk ${JDK_PATH} \
-  --slave /usr/bin/jre_1.8.0 jre_1.8.0 ${JDK_PATH}/jre \
-  --slave /usr/bin/jre_openjdk jre_openjdk ${JDK_PATH}/jre
+  --slave /usr/bin/jre jre ${JDK_LINKS_PATH}
+alternatives --install /usr/bin/javac javac ${JDK_PATH}/bin/javac 1 \
+  --slave /usr/bin/java_sdk java_sdk ${JDK_LINKS_PATH}
 
 ln -s /etc/alternatives/java_sdk /usr/lib/jvm/java
 ln -s /etc/alternatives/java_sdk_1.8.0 /usr/lib/jvm/java-1.8.0
 ln -s /etc/alternatives/java_sdk_1.8.0_openjdk /usr/lib/jvm/java-1.8.0-openjdk
 ln -s /etc/alternatives/java_sdk_openjdk /usr/lib/jvm/java-openjdk
-ln -s /etc/alternatives/jre_1.8.0 /usr/lib/jvm/jre
+ln -s /etc/alternatives/jre /usr/lib/jvm/jre
 ln -s /etc/alternatives/jre_1.8.0 /usr/lib/jvm/jre-1.8.0
 ln -s /etc/alternatives/jre_openjdk /usr/lib/jvm/jre-openjdk
 
